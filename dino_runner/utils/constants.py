b67fa33bf190204d6ba5ccb459732afa9c1c634f
davi-debug-blip-69 <davirp2009@gmail.com>
2022-10-24 19:04:52 -0300

    Initial

---
+++ dino_runner/utils/constants.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+
+# Global constants
+TITLE = "Chrome Dino Runner"
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+FPS = 30
+IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
+
+# Assets constants
+
